2f77155390cc067b02d14d0fabad01239cd24d42
Keith Brings <keith.brings@noizu.com>
2018-01-05 17:45:10 +0700

    version increment

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Noizu.SimplePool.Mixfile do
 
   def project do
     [app: :noizu_simple_pool,
-     version: "1.3.4",
+     version: "1.3.5",
      elixir: "~> 1.4",
      package: package(),
      deps: deps(),
